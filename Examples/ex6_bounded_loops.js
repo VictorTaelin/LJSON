8b7a9c362cd1e8687e2bbb5d16d859ab4f4b204d
Victor Hernandes Silva  Maia <srvictormaia@gmail.com>
2015-10-20 11:27:36 -0200

    bounded loops example

---
+++ Examples/ex6_bounded_loops.js
@@ -0,0 +1,29 @@
+LJSON = require("./../LJSON.js");
+
+// Lets create an environment with only 2 functions,
+// inc (adds 1 to a number)
+// loop (bounded for-loop with max depth of 1000)
+var lib = {
+    inc  : function(a){ return a + 1; },
+    loop : function(limit,state,update){
+        limit = Math.min(limit, 1000); // No more than 1000 loops
+        for (var i=0; i<limit; ++i)
+            state = update(state);
+        return state;
+    }
+};
+
+// With those primitives, you can encode a function
+// that adds 10 to a number:
+function add10($, x){
+    return $("loop", 10, x, function(x){
+        return $("inc",x);
+    });
+};
+
+// Which works as expected:
+var add10Str = LJSON.stringify(add10);
+var add10Val = LJSON.parseWithLib(lib, add10Str);
+
+console.log("add10 stringified : "+add10Str);
+console.log("add10 to 5        : "+add10Val(5));
